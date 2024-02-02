6aabd657de95565dd4c8c216dd0534a75b90ca96
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-01 19:15:22 -0500

    refactored book models to ships

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,35 +1,35 @@
-export const getSavedBookIds = () => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
+export const getSavedShipWreckIds = () => {
+  const savedShipWreckIds = localStorage.getItem('saved_shipWrecks')
+    ? JSON.parse(localStorage.getItem('saved_shipWrecks'))
     : [];
 
-  return savedBookIds;
-}; // checks if there is any data saved under the key 'saved_books' in local storage.
-// If data is found, it parses and returns the array of saved book IDs; otherwise, it returns an empty array.
+  return savedShipWreckIds;
+}; // checks if there is any data saved under the key 'saved_shipWrecks' in local storage.
+// If data is found, it parses and returns the array of saved shipWreck IDs; otherwise, it returns an empty array.
 
-export const saveBookIds = (bookIdArr) => {
-  if (bookIdArr.length) {
-    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
+export const saveShipWreckIds = (shipWreckIdArr) => {
+  if (shipWreckIdArr.length) {
+    localStorage.setItem('saved_shipWrecks', JSON.stringify(shipWreckIdArr));
   } else {
-    localStorage.removeItem('saved_books');
+    localStorage.removeItem('saved_shipWrecks');
   }
-}; // checks if the array has any elements. If it does. it saves the array to local storage under "saved_books" after converting to JSON
-// If the array is empty, it removes the 'saved_books' key from local storage.
+}; // checks if the array has any elements. If it does. it saves the array to local storage under "saved_shipWrecks" after converting to JSON
+// If the array is empty, it removes the 'saved_shipWrecks' key from local storage.
 
-export const removeBookId = (bookId) => {
-  const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
+export const removeShipWreckId = (shipWreckId) => {
+  const savedShipWreckIds = localStorage.getItem('saved_shipWrecks')
+    ? JSON.parse(localStorage.getItem('saved_shipWrecks'))
     : null;
 
-  if (!savedBookIds) {
+  if (!savedShipWreckIds) {
     return false;
   }
 
-  const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
-  localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
+  const updatedSavedShipWreckIds = savedShipWreckIds?.filter((savedShipWreckId) => savedShipWreckId !== shipWreckId);
+  localStorage.setItem('saved_shipWrecks', JSON.stringify(updatedSavedShipWreckIds));
 
   return true;
-}; // retrieves the current saved book IDS from local storage
-// if no saved book ids, then returns false
-// filters out the specified bookID from the array
+}; // retrieves the current saved shipWreck IDS from local storage
+// if no saved shipWreck ids, then returns false
+// filters out the specified shipWreckID from the array
 // saves the updated array in local storage
