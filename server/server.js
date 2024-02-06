a29bb85376869e3e867f7806a70c7b485ba66556
christiecamp <christiecamphoto@gmail.com>
2024-02-06 14:13:40 -0500

    changes to server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 //import ApolloServer
-const { ApolloServer } = require("@apollo/server");
-const path = require("path");
+const { ApolloServer } = require('@apollo/server');
+const path = require('path');
 //import middleware
 const { authMiddleware } = require("./utils/auth");
+const { expressMiddleware } = require('@apollo/server/express4');
 // Import the two parts of a GraphQL schema
-const { typeDefs, resolvers } = require("./schemas");
-const booty = require("./config/connection");
+const { typeDefs, resolvers } = require('./schemas');
+const booty = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const pirate = express();
@@ -24,20 +25,20 @@ pirate.use(express.urlencoded({ extended: true }));
 pirate.use(express.json());
 
 //serve up static assets in production
-if (process.env.NODE_ENV === "production") {
-  pirate.use(express.static(path.join(__dirname, "../client/dist")));
+if (process.env.NODE_ENV === 'production') {
+  pirate.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-pirate.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+pirate.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 //integrate Apollo server with Express application as middleware
-pirate.get('/graphql', expressMiddleware(server, {
+pirate.get('/graphql', expressMiddleware(shipwreck, {
   context: authMiddleware
 }));
 
-booty.once("open", () => {
+booty.once('open', () => {
   pirate.listen(PORT, () => {
     console.log(`
 ============================================================
@@ -56,16 +57,19 @@ booty.once("open", () => {
 88888888888888888888P" od88888888bo "98888888888888888888888
 888888888888888888   d88888888888888b   88888888888888888888
 8888888888888888888oo8888888888888888oo888888888888888888888
-888888888888888888888888888888888888888888888treasure9888888
+888888888888888888888888888888888888888wreckdtreasure9888888
 
               ==============================
                 Ahoy matey! join the crew!
+
+                 x     *     *     *   x
+
                  http://localhost:${PORT}
-              ==============================      
+              ==============================
 
-        graphql path: 
-        http://localhost:${PORT}${server.graphqlPath}
 
+              graphql path:
+              http://localhost:${PORT}/graphql
       `);    
     });
   });
