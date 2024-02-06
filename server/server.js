50c763862b32e82bf11722e563038b5b9c85ae1e
christiecamp <christiecamphoto@gmail.com>
2024-02-06 11:40:03 -0500

    updates to server.js file and removing existing elements that do not apply to graphql

--- server/server.js
+++ server/server.js
@@ -1,44 +1,71 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-
-// Import the ApolloServer Class
+//import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+//import middleware
 const { authMiddleware } = require("./utils/auth");
+
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require("./schemas");
+const booty = require("./config/connection");
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
-// Create a new instance of an Apollo server with the GraphQL schema
-const server = new ApolloServer({
+const pirate = express();
+const shipwreck = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+pirate.use(express.urlencoded({ extended: true }));
+pirate.use(express.json());
 
-// if we're in production, serve client/build as static assets
+//serve up static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
+  pirate.use(express.static(path.join(__dirname, "../client/dist")));
 }
+pirate.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+//create instance of ApolloServer and pass in schema data
+const startApolloServer = async () => {
+  await shipwreck.start();
 
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+//integrate Apollo server with Express application as middleware
+shipwreck.applyMiddleware({ pirate });
+
+booty.once("open", () => {
+  pirate.listen(PORT, () => {
+    console.log(`
+============================================================
+               W R E C K ' D   T R E A S U R E 
+============================================================
+888888888888888888888888888888888888888888888888888888888888
+888888888888888888888888888888888888888888888888888888888888
+8888888888888888888888888P""  ""9888888888888888888888888888
+8888888888888888P"88888P          988888"9888888888888888888
+8888888888888888  "9888            888P"  888888888888888888
+888888888888888888bo "9  d8o  o8b  P" od88888888888888888888
+888888888888888888888bob 98"  "8P dod88888888888888888888888
+888888888888888888888888    db    88888888888888888888888888
+88888888888888888888888888      8888888888888888888888888888
+88888888888888888888888P"9bo  odP"98888888888888888888888888
+88888888888888888888P" od88888888bo "98888888888888888888888
+888888888888888888   d88888888888888b   88888888888888888888
+8888888888888888888oo8888888888888888oo888888888888888888888
+888888888888888888888888888888888888888888888treasure9888888
+
+              ==============================
+                Ahoy matey! join the crew!
+                 http://localhost:${PORT}
+              ==============================      
+
+            graphql path: 
+            http://localhost:${PORT}${server.graphqlPath}
+
+      `);    
     });
   });
 };
