5b7795fff3e44dc5ca8a4b9fc40a2269df859411
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-07 17:02:39 -0500

    added key rarity and treasure

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,75 +1,77 @@
-const { User } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-          console.log("queryME context:", context)
-            if (context.user) {
-             const data = await User.findOne({ _id: context.user._id }).select('-__v -password');
-              return data;
-            } else {
-            throw AuthenticationError;
-            }
-        },
-    },
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-          const user = await User.create({ username, email, password });
-          const token = signToken(user);
-          return { token, user };
-        },
-        login: async (parent, { email, password }) => {
-          const user = await User.findOne({ email });
-    
-          if (!user) {
-            throw AuthenticationError;
-          }
-    
-          const correctPw = await user.isCorrectPassword(password);
-    
-          if (!correctPw) {
-            throw new AuthenticationError;
-          }
-    
-          const token = signToken(user);
-          console.log('Logged IN')
-
-          return { token, user };
-        },
-        logout: async (parent, args, context) => {
-          if(context.user) {
-            context.user = null;
-            return {success: true, message: 'Logout Successful'}
-          } else {
-            throw AuthenticationError
-          }
+  Query: {
+    me: async (parent, args, context) => {
+      console.log("queryME context:", context);
+      if (context.user) {
+        const data = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
         
-      },
-        saveShipWreck: async (parent, { newShipWreck }, context) => {
-          console.log("user context", context.user)
-          if (context.user) {
-            const updatedUser = await User.findByIdAndUpdate(
-              { _id: context.user._id },
-              { $push: { savedShipWrecks: newShipWreck }},
-              { new: true }
-            );
-            return updatedUser;
-          }
-          throw AuthenticationError;
-        },
-        removeShipWreck: async (parent, { shipWreckId }, context) => {
-          if (context.user) {
-            const updatedUser = await User.findByIdAndUpdate(
-              { _id: context.user._id },
-              { $pull: { savedShipWrecks: { shipWreckId }}},
-              { new: true }
-            );
-            return updatedUser;
-          }
-          throw AuthenticationError;
-        },
-    }
+        return data;
+      } else {
+        throw AuthenticationError;
+      }
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError();
+      }
+
+      const token = signToken(user);
+      console.log("Logged IN");
+
+      return { token, user };
+    },
+    logout: async (parent, args, context) => {
+      if (context.user) {
+        context.user = null;
+        return { success: true, message: "Logout Successful" };
+      } else {
+        throw AuthenticationError;
+      }
+    },
+    saveShipWreck: async (parent, { newShipWreck }, context) => {
+      console.log("user context", context.user);
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedShipWrecks: newShipWreck } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+    removeShipWreck: async (parent, { shipWreckId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedShipWrecks: { shipWreckId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+  },
 };
 
 module.exports = resolvers;
