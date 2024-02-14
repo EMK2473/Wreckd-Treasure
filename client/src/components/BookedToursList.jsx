83b8794989b69bffa7f182ad722b8fe049279716
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-02-14 14:43:46 -0500

    fixed buttons and response for mobile

--- client/src/components/BookedToursList.jsx
+++ client/src/components/BookedToursList.jsx
@@ -17,22 +17,22 @@ const BookedToursList = () => {
   const [removeTourMutation] = useMutation(REMOVE_TOUR, {
     refetchQueries: [{ query: GET_ME }],
   });
-const [buttonColor, setButtonColor] = useState("#baf0f0"); 
-const [borderColor, setBorderColor] = useState("#287382"); 
-const [fontColor, setFontColor] = useState("#062c33"); 
+  const [buttonColor, setButtonColor] = useState("#baf0f0");
+  const [borderColor, setBorderColor] = useState("#287382");
+  const [fontColor, setFontColor] = useState("#062c33");
 
-const handleButtonClick = () => {
-  // Toggle button colors on click
-  const newButtonColor = buttonColor === "#baf0f0" ? "#287382" : "#baf0f0";
-  const newBorderColor = borderColor === "#287382" ? "#baf0f0" : "#287382";
-  const newFontColor = fontColor === "#062c33" ? "#F4CB5C" : "#062c33";
-  setButtonColor(newButtonColor);
-  setBorderColor(newBorderColor);
-  setFontColor(newFontColor);
-  
-  // Toggle the state of 'open'
-  setOpen(!open);
-};
+  const handleButtonClick = () => {
+    // Toggle button colors on click
+    const newButtonColor = buttonColor === "#baf0f0" ? "#287382" : "#baf0f0";
+    const newBorderColor = borderColor === "#287382" ? "#baf0f0" : "#287382";
+    const newFontColor = fontColor === "#062c33" ? "#F4CB5C" : "#062c33";
+    setButtonColor(newButtonColor);
+    setBorderColor(newBorderColor);
+    setFontColor(newFontColor);
+
+    // Toggle the state of 'open'
+    setOpen(!open);
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -61,13 +61,13 @@ const handleButtonClick = () => {
       title: "Item",
       dataIndex: "tourName",
       key: "tourName",
-      ellipsis: true, 
+      ellipsis: true,
     },
     {
       title: "Cost",
       dataIndex: "tourCost",
       key: "tourCost",
-      ellipsis: true, 
+      ellipsis: true,
     },
     {
       title: "Actions",
@@ -129,25 +129,25 @@ const handleButtonClick = () => {
         zIndex: "3",
       }}
     >
-<Button
-  style={{
-    cursor: "pointer",
-    backgroundColor: buttonColor, 
-    color: fontColor, 
-    borderRadius: "5px 5px 5px 5px",
-    border: `solid 2px ${borderColor}`, 
-    width: "120px",
-    textAlign: "center",
-    marginRight: "20px",
-    fontSize: "12px",
-    paddingBottom: "30px",
-    paddingTop: "10px",
-    fontFamily: "Pirata One, sans-serif",
-  }}
-  onClick={handleButtonClick}
->
-  {open ? `Hide 🛒 Cart` : `Show 🛒 Cart`}
-</Button>
+      <Button
+        style={{
+          cursor: "pointer",
+          backgroundColor: buttonColor,
+          color: fontColor,
+          borderRadius: "5px 5px 5px 5px",
+          border: `solid 2px ${borderColor}`,
+          width: "120px",
+          textAlign: "center",
+          marginRight: "20px",
+          fontSize: "12px",
+          paddingBottom: "30px",
+          paddingTop: "10px",
+          fontFamily: "Pirata One, sans-serif",
+        }}
+        onClick={handleButtonClick}
+      >
+        {open ? `Hide 🛒 Cart` : `Show 🛒 Cart`}
+      </Button>
       {open && (
         <Collapse in={open}>
           <Card
@@ -155,35 +155,34 @@ const handleButtonClick = () => {
             style={{
               background: "none",
               boxShadow: "none",
-              overflow: "auto", 
+              overflow: "auto",
             }}
           >
             <Collapse>
-            <Panel
-  header={
-    <span style={{ color: "#F4CB5C", textAlign: "center" }}>
-      {`${username}'s 🛒 Cart`}
-    </span>
-  }
-  key="1"
-  style={{
-    borderRadius: "10px",
-    backgroundColor: "#56727D",
-    maxWidth: "400px", 
-    width: "100%",
-    overflow: "auto", 
-    margin: "0 auto", 
-    maxHeight: "70vh", 
-  }}
->
-
+              <Panel
+                header={
+                  <span style={{ color: "#F4CB5C", textAlign: "center" }}>
+                    {`${username}'s 🛒 Cart`}
+                  </span>
+                }
+                key="1"
+                style={{
+                  borderRadius: "10px",
+                  backgroundColor: "#56727D",
+                  maxWidth: "400px",
+                  width: "100%",
+                  overflow: "auto",
+                  margin: "0 auto",
+                  maxHeight: "70vh",
+                }}
+              >
                 <div className="custom-collapse-body">
                   <Table
                     columns={columns}
                     dataSource={dataSource}
                     pagination={false}
                     showHeader={false}
-                    scroll={{ y: 200 }} 
+                    scroll={{ y: 200 }}
                   />
                   <div style={{ marginTop: "20px" }}>
                     <p
